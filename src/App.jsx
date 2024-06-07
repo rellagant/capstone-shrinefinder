7d39b629264ca95bae4fce847b2daa1feb94125c
rellagant <barbara.cs@gmail.com>
2024-06-06 23:25:31 -0400

    added initial route pathing; added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.scss';
+import "./App.scss";
 import "./styles/partials/_globals.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "../src/pages/HomePage/HomePage.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
     <>
-   <h1>初めまして,レラです！よろしくおネアいたします！</h1>
-   </>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
