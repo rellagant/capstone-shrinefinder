900d4508ffb7554e9aeeb7b755f8295fa98bdea9
rellagant <barbara.cs@gmail.com>
2024-06-07 00:59:37 -0400

    refined routing; moved header and footer into homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,15 @@ import "./App.scss";
 import "./styles/partials/_globals.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../src/pages/HomePage/HomePage.jsx";
-import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-      <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,13 @@
-import "./HomePage.scss"
-import Header from "../../components/Header/Header"
+import "./HomePage.scss";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 export default function HomePage() {
-    <>
+  return (
+    <div className="home-page">
       <Header />
-      </>
+
+      <Footer />
+    </div>
+  );
 }
