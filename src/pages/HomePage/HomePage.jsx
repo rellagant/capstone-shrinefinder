bb6848c1ce6907ff3d6977418498db58f3b890df
rellagant <barbara.cs@gmail.com>
2024-06-07 21:21:07 -0400

    added shrine roulette component to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import "./HomePage.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import ShrineRoulette from "../../components/ShrineRoulette/ShrineRoulette";
 
 export default function HomePage() {
   return (
     <div className="home-page">
       <Header />
-
+      <ShrineRoulette />
       <Footer />
     </div>
   );
