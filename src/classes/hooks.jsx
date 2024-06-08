579ea1169b28f896fa18be78e4fa571caf6fb824
rellagant <barbara.cs@gmail.com>
2024-06-07 21:20:23 -0400

    created hook for random endpoint call in shrine roulette component

---
+++ src/classes/hooks.jsx
@@ -0,0 +1,31 @@
+import { ShrineFinderApi } from "./shrinesapi";
+import { useState, useEffect } from "react";
+
+const api = new ShrineFinderApi();
+
+const useRandomShrines = () => {
+  const [randomShrine, setRandomShrine] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchRandomShrine = async () => {
+      setLoading(true);
+      try {
+        const response = await api.getRandomShrine();
+        console.log(response);
+        setRandomShrine(response);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        setError(error);
+      }
+    };
+
+    fetchRandomShrine();
+  }, []);
+
+  return { randomShrine, loading, error };
+};
+
+export default useRandomShrines;
