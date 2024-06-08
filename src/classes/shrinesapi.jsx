1b01a16623b3fcfddbd482c6b556231a3868a211
rellagant <barbara.cs@gmail.com>
2024-06-07 21:19:22 -0400

    refined api class; removed strict mode

--- src/classes/shrinesapi.jsx
+++ src/classes/shrinesapi.jsx
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
-console.log(API_URL);
+
 
 export class ShrineFinderApi {
   constructor() {
     this.baseURL = API_URL;
-    console.log(this.baseURL);
   }
 
   async getShrines() {
@@ -19,22 +18,22 @@ export class ShrineFinderApi {
     }
   }
 
-  async getShrineById(id) {
-    try {
-      const response = await axios.get(`${this.baseURL}/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error(`Error fetching shrine by id ${id}:`, error);
-      throw error;
-    }
-  }
-
   async getRandomShrine() {
     try {
       const response = await axios.get(`${this.baseURL}/random`);
       return response.data;
     } catch (error) {
       console.error("Error fetching random shrine:", error);
+      throw error;
+    }
+  }
+
+  async getShrineById(id) {
+    try {
+      const response = await axios.get(`${this.baseURL}/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error(`Error fetching shrine by id ${id}:`, error);
       throw error;
     }
   }
