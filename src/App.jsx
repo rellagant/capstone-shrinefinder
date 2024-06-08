b06dcb674d5898e100195ac645f22d204007114a
rellagant <barbara.cs@gmail.com>
2024-06-08 13:22:30 -0400

    created prefecture page and routed in app.jsx; deleted errant shrine roulette page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.scss";
 import "./styles/partials/_globals.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../src/pages/HomePage/HomePage.jsx";
+import Prefectures from '../src/pages/Prefectures/Prefectures.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/prefectures/:id" element={<Prefectures />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Prefectures/Prefectures.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import "./Prefectures.scss";
+import Footer from "../../components/Footer/Footer";
+
+export default function Pefectures() {
+  return (
+    <>
+    <Header />
+    <h1>これは県のページです </h1>
+    <Footer />
+    </>
+  )
+}
