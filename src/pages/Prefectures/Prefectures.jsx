05d7730258b358021ed51720075a0c1c05b12161
rellagant <barbara.cs@gmail.com>
2024-06-09 19:38:00 -0400

    refined params; refactored useEffect and api call; added api url client side in case host address changes

--- src/pages/Prefectures/Prefectures.jsx
+++ src/pages/Prefectures/Prefectures.jsx
@@ -1,78 +1,49 @@
 import Header from "../../components/Header/Header";
 import "./Prefectures.scss";
 import Footer from "../../components/Footer/Footer";
-import { useEffect} from "react";
+import { ShrineFinderApi } from "../../utils/shrinesapi";
+import { useState, useEffect} from "react";
 import { useParams, useLocation, Link } from "react-router-dom";
-import { useShrineByPrefecture } from "../../utils/hooks";
-// import ShrineMain from "../../components/ShrineMain/ShrineMain";
 
-// const api = new ShrineFinderApi();
+const api = new ShrineFinderApi();
 
 export default function Prefectures() {
-  const { id } = useParams(); // Get the prefecture from the URL
+  const { prefecture } = useParams(); 
   const location = useLocation();
-  const shrineData = useShrineByPrefecture(id);
-  // console.log(shrineData);
-  // const [shrineData, setShrineData] = useState([]);
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState(null);
+  const [shrines, setShrines] = useState([]);
+
+  useEffect(() => {
+    const fetchShrines = async () => {
+      try {
+        const shrinesData = await api.getShrinesByPrefecture(prefecture);
+        setShrines(shrinesData);
+      } catch (error) {
+        console.error("Error fetching shrines by prefecture:", error);
+      }
+    };
+    fetchShrines();
+  }, [prefecture]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
-  // useEffect(() => {
-  //   const fetchShrinesByPrefecture = async () => {
-  //     setLoading(true);
-  //     try {
-  //       const shrinesData = await api.getShrinesByPrefecture();
-  //       setShrineData(shrinesData);
-  //       setLoading(false);
-  //     } catch (error) {
-  //       setLoading(false);
-  //       setError(error);
-  //     }
-  //   };
-
-  //   fetchShrinesByPrefecture();
-  // }, []);
-
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
-
-  // if (error) {
-  //   return <div>Error: {error.message}</div>;
-  // }
-
-  // const shrines = shrineData[prefecture] || [];
-  // console.log(id);
-  // console.log(shrineData);
-  // const shrine = shrineData.find((prefecture) => prefecture === id)
-  // console.log(shrine);
   return (
     <>
       <Header />
-      {/* <ShrineMain /> */}
       <div className="shrines">
-        <h1 className="shrines__header">Shrines and Power Spots in {id}</h1>
-        <div> 
-          {shrineData
-            .filter((prefecture) => prefecture.prefecture === id) // Filter by the current prefecture
-            .map((prefecture) => (
+        <h1 className="shrines__header">Shrines and Power Spots in {prefecture}</h1>
+        <div key={prefecture}>
+          {shrines.map((shrine) => (
               
-              <div key={prefecture.prefecture}>
-                {prefecture.shrines.map((shrine) => (
               
                   <div key={shrine.id}>
                     <h4>{shrine.name}</h4>
                     <p>{shrine.city}</p>
                     <Link key={shrine.id} to={`/shrine/${shrine.id}`}>
-                    <img src={shrine.image} alt={`${shrine.name} shrine`} />
+                    <img src={api.baseURL+shrine.image} alt={`${shrine.name} shrine`} />
                     </Link>
                   </div>
-                ))}
-              </div>
             ))}
         </div>
       </div>
