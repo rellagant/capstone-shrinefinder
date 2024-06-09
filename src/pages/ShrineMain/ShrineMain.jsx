a25ee7b489d296945a144d91929f4825ca110cb5
rellagant <barbara.cs@gmail.com>
2024-06-09 19:39:08 -0400

    added useEffect and api call; mapped data into html structure; added api url client side

--- src/pages/ShrineMain/ShrineMain.jsx
+++ src/pages/ShrineMain/ShrineMain.jsx
@@ -1,50 +1,89 @@
 import "./ShrineMain.scss";
-import shrine from "../../assets/images/torii.jpg";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import { useState, useEffect } from "react";
+import { ShrineFinderApi } from "../../utils/shrinesapi";
+import { useParams, useLocation } from "react-router-dom";
+
+const api = new ShrineFinderApi();
 
 export default function ShrineMain() {
+  const { shrineId } = useParams();
+  const location = useLocation();
+
+  const [shrine, setShrine] = useState([]);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchShrineData = async () => {
+      try {
+        const shrineData = await api.getShrineById(shrineId);
+        const reviewData = await api.getReviewsByShrineId(shrineId);
+
+        setShrine(shrineData);
+        setReviews(reviewData);
+      } catch (error) {
+        console.error("Error fetching shrine data by id:", error);
+      }
+    };
+    fetchShrineData();
+  }, [shrineId]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  if (shrine.length === 0) {
+    return;
+  }
+
   return (
     <>
-    <Header />
+      <Header />
       <section className="landmark">
-        <img className="landmark__image" src={shrine} alt="torii" />
-        <div className="landmark__name">Reigandō</div>
+        <img
+          className="landmark__image"
+          src={api.baseURL + shrine.image}
+          alt="torii"
+        />
+        <div className="landmark__name">{shrine.name}</div>
         <div className="landmark__label">City</div>
-        <div className="landmark__city">Kumamoto</div>
-        <p className="landmark__description">
-          "Reigando Cave, also known as Reigando (霊厳洞), is a historical
-          landmark steeped in the legend of the famed swordsman Miyamoto
-          Musashi. Nestled amidst tangerine orchards, this cave served as a
-          retreat for Musashi during his later years. Here, he is believed to
-          have penned his legendary book, The Book of Five Rings, a treatise on
-          swordsmanship and strategy. Visitors can explore the serene cave
-          surroundings and witness the hundreds of weathered stone statues
-          (Gohyaku Rakan) representing the 500 disciples of Buddha who attained
-          enlightenment. Pay homage at the enshrined statue of Kannon, the
-          Bodhisattva of Compassion. Reigandō offers a glimpse into the life and
-          legacy of Miyamoto Musashi, a figure who continues to inspire
-          swordsmen and strategists alike. There's a great restaurant halfway up
-          to the cave, try the curry!"
-        </p>
+        <div className="landmark__city">{shrine.city}</div>
+        <p className="landmark__description">{shrine.description}</p>
         <div className="landmark__label">Enshrined Deities</div>
-        <div className="landmark__deities">Kannon</div>
+        <div className="landmark__deities">
+          {shrine.deities.map((deity, i) => (
+            <div className="landmark__deity" key={i}>
+              {deity}
+            </div>
+          ))}
+        </div>
         <div className="landmark__label">Features</div>
-        <p className="landmark__features">
-          Retreat of the legendary swordsman Miyamoto Musashi, Location where
-          he wrote The Book of Five Rings, Hundreds of weathered stone statues
-          (Gohyaku Rakan), Statue of Kannon, the Bodhisattva of Compassion
-        </p>
+        <div className="landmark__features">
+          {shrine.features.map((feature, i) => (
+            <div className="landmark__feature" key={i}>
+              {feature}
+            </div>
+          ))}
+        </div>
         <div className="landmark__label">Souvenirs</div>
-        <div className="landmark__souvenirs">"Unknown"</div>
+        <div className="landmark__souvenirs">
+          {shrine.souvenirs_and_charms.map((souvenir, i) => (
+            <div className="landmark__souvenir" key={i}>
+              {souvenir}
+            </div>
+          ))}
+        </div>
         <div className="landmark__reviews reviews">
-        {/* <div className="landmark__label">Review</div> */}
-        <div className="landmark__label">Rating</div>
-          <p className="reviews__rating">5</p>
-       
-          <p className="reviews__comment">"Worth the hike, it provides solitude and gorgeous views. If you visit at the right time hundreds of golden dragonflies will lead the way."
-          </p>
-          <p className="reviews__reviewer">-Brian Station</p>
+          {reviews.map((review, i) => (
+            <div className="landmark__review" key={i}>
+              <div className="landmark__label">Rating</div>
+              <p className="reviews__rating">{review.rating}</p>
+
+              <p className="reviews__comment">{review.comment}</p>
+              <p className="reviews__reviewer">{review.reviewer}</p>
+            </div>
+          ))}
         </div>
       </section>
       <Footer />
