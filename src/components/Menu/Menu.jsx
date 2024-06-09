1c5b389b4bcc3b84467b2b72f0b2a153c9b470f5
rellagant <barbara.cs@gmail.com>
2024-06-09 02:52:39 -0400

    changed mapping structure post use effect

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,23 +5,53 @@ import kumamoto from "../../assets/images/kumamoto.png";
 import osaka from "../../assets/images/osaka.png";
 import nara from "../../assets/images/nara.png";
 import kyoto from "../../assets/images/kyoto.png";
+import { useState, useEffect } from "react";
+import { ShrineFinderApi } from "../../classes/shrinesapi";
+
+const api = new ShrineFinderApi();
 
 export default function Menu() {
-  const prefectures = [
-    { id: "okayama", image: okayama, alt: "okayama prefecture" },
-    { id: "osaka", image: osaka, alt: "osaka prefecture" },
-    { id: "nara", image: nara, alt: "nara prefecture" },
-    { id: "kumamoto", image: kumamoto, alt: "kumamoto prefecture" },
-    { id: "kyoto", image: kyoto, alt: "kyoto" },
-  ];
+  // const [shrines, setShrines] = useState([]);
+  const [prefectures, setPrefectures] = useState([]);
+
+  useEffect(() => {
+    const fetchShrines = async () => {
+      try {
+        const shrinesData = await api.getShrines();
+        // setShrines(shrinesData);
+
+        const uniquePrefectures = [
+          ...new Set(shrinesData.map((shrine) => shrine.prefecture)),
+        ];
+
+        setPrefectures(uniquePrefectures);
+      } catch (error) {
+        console.error("Error fetching shrines:", error);
+      }
+    };
+    fetchShrines();
+  }, []);
+
+  const prefectureImages = {
+    okayama,
+    kumamoto,
+    osaka,
+    nara,
+    kyoto,
+  };
+
   return (
     <>
       <div className="menu">
         <h2 className="menu__title">Choose a Prefecture</h2>
         <div className="menu__map">
           {prefectures.map((prefecture) => (
-            <Link key={prefecture.id} to={`/prefectures/${prefecture.id}`}>
-              <img src={prefecture.image} alt={prefecture.alt} className="menu__image"/>
+            <Link key={prefecture} to={`/${prefecture}`}>
+              <img
+                src={prefectureImages[prefecture.toLowerCase()]}
+                alt={`${prefecture} prefecture`}
+                className="menu__image"
+              />
             </Link>
           ))}
         </div>
