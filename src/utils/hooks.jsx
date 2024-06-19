0ac8e4f3fc7e977f0d0029c60e07791b6332ab07
rellagant <barbara.cs@gmail.com>
2024-06-19 15:27:08 -0400

    adjusted hook to include Get Lucky button logic for the header

--- src/utils/hooks.jsx
+++ src/utils/hooks.jsx
@@ -1,5 +1,6 @@
 import { ShrineFinderApi } from "./shrinesapi";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const api = new ShrineFinderApi();
 
@@ -7,23 +8,48 @@ export const useRandomShrines = () => {
   const [randomShrine, setRandomShrine] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchRandomShrine = async () => {
-      setLoading(true);
-      try {
-        const response = await api.getRandomShrine();
-        console.log(response);
-        setRandomShrine(response);
+  const fetchRandomShrine = async () => {
+    setLoading(true);
+    try {
+      const response = await api.getRandomShrine();
+      console.log(response);
+      setRandomShrine(response);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+    }
+  };
+  console.log(randomShrine);
+
+  const handleGetLucky = async () => {
+    setLoading(true);
+    try {
+      const response = await api.getRandomShrine();
+      const randomShrine = response;
+
+      if (!randomShrine || !randomShrine.id) {
+        setError("No shrine was found today");
         setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        setError(error);
+        return;
       }
-    };
 
-    fetchRandomShrine();
-  }, []);
+      setRandomShrine(randomShrine);
+      navigate(`/shrine/${randomShrine.id}`);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
 
-  return { randomShrine, loading, error };
+  return {
+    randomShrine,
+    loading,
+    error,
+    fetchRandomShrine,
+    handleGetLucky,
+  };
 };
