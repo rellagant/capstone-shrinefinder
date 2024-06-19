c77e5fe08d7635d7a87b66a1c97d7508f67d11b7
rellagant <barbara.cs@gmail.com>
2024-06-19 16:11:30 -0400

    moved header component into app/routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import "./styles/partials/_globals.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header.jsx";
 import HomePage from "../src/pages/HomePage/HomePage.jsx";
 import Prefectures from '../src/pages/Prefectures/Prefectures.jsx'
 import ShrineMain from "./pages/ShrineMain/ShrineMain.jsx";
 function App() {
   return (
     <BrowserRouter>
+         <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shrines/:prefecture" element={<Prefectures />} />
